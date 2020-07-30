6cf88a9433efb59e9d554ded0058f9c0c7dfc088
busybasi <40594034+busybasi@users.noreply.github.com>
2020-07-31 05:22:05 +0800

    Setup separate asset routing for subjects.

--- index.js
+++ index.js
@@ -3,8 +3,20 @@ const path = require('path')
 
 const app = express();
 
+app.post("/subjects/biology", function(req, res, next) {
+    req.method = 'GET';
+    next();
+});
+
+app.post("/subjects/chemistry", function(req, res, next) {
+    req.method = 'GET';
+    next();
+});
+
 // Set static folder
-app.use(express.static(path.join(__dirname, 'public')));
+app.use('/', express.static(path.join(__dirname, 'public')));
+app.use('/subjects/biology', express.static(path.join(__dirname, 'public/assets/biology')));
+app.use('/subjects/chemistry', express.static(path.join(__dirname, 'public/assets/chemistry')));
 
 const PORT = process.env.PORT || 5000;
 
