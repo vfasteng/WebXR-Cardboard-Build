0972cf00342516c2791c7b795fc4198edeb9609b
busybasi <40594034+busybasi@users.noreply.github.com>
2020-07-31 16:37:43 +0800

    Setup login, signup and home pages.

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
+// Import required modules.
 const express = require('express');
 const path = require('path')
 
+// Create app.
 const app = express();
 
-app.post("/subjects/biology", function(req, res, next) {
-    req.method = 'GET';
-    next();
-});
+// Setup some routing override.
+const accessRouteOverrideMapping = {
+    '/login': [],
+    '/register': [],
+    '/home': ['biology', 'chemistry', 'physics', 'math', 'earthscience']
+}
 
-app.post("/subjects/chemistry", function(req, res, next) {
-    req.method = 'GET';
-    next();
-});
+const acccessPointOverride = accessPoint => {
+    app.post(accessPoint, (req, res, next) => {
+        req.method = 'GET';
+        next();
+    });
+}
 
-// Set static folder
+for (const key in accessRouteOverrideMapping) {
+    const nodes = accessRouteOverrideMapping[key];
+    if (nodes) {
+        nodes.forEach(node => acccessPointOverride(key + node));
+    }
+    acccessPointOverride(key);
+}
+
+// Set static resources.
 app.use('/', express.static(path.join(__dirname, 'public')));
-app.use('/subjects/biology', express.static(path.join(__dirname, 'public/assets/biology')));
-app.use('/subjects/chemistry', express.static(path.join(__dirname, 'public/assets/chemistry')));
+app.use('/login', express.static(path.join(__dirname, 'public/routes/login')));
+app.use('/register', express.static(path.join(__dirname, 'public/routes/register')));
+app.use('/home', express.static(path.join(__dirname, 'public/routes/home')));
+accessRouteOverrideMapping['/home'].forEach(
+    node => app.use('/home/' + node, express.static(path.join(__dirname, 'public/assets/' + node)))
+);
 
+// Set port.
 const PORT = process.env.PORT || 5000;
 
+// Start server.
 app.listen(PORT, () => {
     console.log(`Server listening to port: ${PORT}.`)
 });
